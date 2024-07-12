ba4f91a04f2f48f1cb1706eaef71a1f125bbb575
Alessandro Romano <ale.romano.esterno@almaviva.it>
2024-07-12 12:35:39 +0200

    created kanban task board

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "@/pages/company/create";
 import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
 
 function App() {
   return (
@@ -63,6 +64,9 @@ function App() {
                         <Route path='new' element={<Create />} />
                         <Route path='edit/:id' element={<Edit />} />
                     </Route>
+                </Route>
+                <Route path="/tasks">
+                        <Route index element={<List />} />
                 </Route>
               </Routes>
               <RefineKbar />
